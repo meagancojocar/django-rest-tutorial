0fd28457e25558a01c65477b85ff635992e130c0
“meagancojocar” <meagan.cojocar@gmail.com>
2019-09-28 13:07:16 +0800

    fixing directory structure

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
